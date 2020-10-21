773c9cc067ec681e18ef6e6123b51c0a03fe2b77
Abir Yusuf <abiryusuf@abirs-mbp.home>
2020-10-21 19:16:22 -0400

    webdriver

---
+++ src/test/java/HelloSelenium2Test.java
@@ -0,0 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloSelenium2Test {
+
+    @BeforeClass
+    public static void setUpClass(){
+        WebDriverManager.chromedriver().setup();
+
+    }
+    @Test
+    public void test1(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+        driver.quit();
+
+    }
+}
--- src/test/java/HelloSeleniumTest.java
+++ src/test/java/HelloSeleniumTest.java
@@ -1,11 +1,20 @@
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloSeleniumTest {
+
+    @Test
     public void firstTest(){
         //Telling the system where to find chromedriver.on windows need to add .exe
-        System.setProperty("webdriver.chromedriver", "resources/chromdriver");
+        System.setProperty("webdriver.chrome.driver", "resource/mac/chromedriver");
+
+//        windows
+       // System.setProperty("webdriver.chrome.driver", "resource.windows/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+        driver.quit();
+
     }
 }
