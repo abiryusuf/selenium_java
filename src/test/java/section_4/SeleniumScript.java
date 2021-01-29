dae658d0a6f42191d6ddbcb681b8955dd8753f88
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-01-29 10:38:04 -0500

    test2

--- src/test/java/section_4/SeleniumScript.java
+++ src/test/java/section_4/SeleniumScript.java
@@ -2,8 +2,12 @@ package section_4;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumScript {
 
@@ -11,12 +15,23 @@ public class SeleniumScript {
 
     @Test
     public void findElement() throws InterruptedException {
-
+        //set the location by file
         //System.setProperty("webdriver.chrome.driver", "resource/mac/chromedriver");
+       // set the location by webdriver manager by maven project
         WebDriverManager.chromedriver().setup();
+        //start session, open browser
         driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com");
+        //Navigate the url
+        driver.get("http://www.saucedemo.com");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("login-button")));
+
+        element.click();
+
+
         Thread.sleep(3000);
+        //close the browser
         driver.quit();
 
     }
