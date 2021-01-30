c00f7107bbc3747d708cf295c63e667e9dd6c359
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-01-30 15:36:27 -0500

    locator css and xpath

--- src/test/java/locating/element/locatorTestDemo.java
+++ src/test/java/locating/element/locatorTestDemo.java
@@ -2,6 +2,7 @@ package locating.element;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -24,12 +25,31 @@ public class locatorTestDemo {
     public void locatorTest() throws InterruptedException {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://www.saucedemo.com");
-        Thread.sleep(3000);
+        Thread.sleep(2000);
         // Use cssSelector
         driver.findElement(By.cssSelector("input#user-name")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("input#password")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector("input#login-button")).click();
+        Thread.sleep(2000);
 
+        //Xpath
+        driver.findElement(By.xpath("//*[contains(text(),'Sauce Labs Bike Light')]")).click();
+        driver.findElement(By.xpath("//*[contains(text(),'ADD TO CART')]")).click();
+        driver.findElement(By.cssSelector("#shopping_cart_container > a > svg > path")).click();
+        driver.findElement(By.xpath("//*[contains(text(),'CHECKOUT')]")).click();
+
+        //Best Locator
+        driver.findElement(By.id("first-name")).sendKeys("abir");
+        driver.findElement(By.id("last-name")).sendKeys("yusuf");
+        driver.findElement(By.id("postal-code")).sendKeys("11435");
+        driver.findElement(By.xpath("//*[@class='btn_primary cart_button'][@type='submit']")).click();
+        driver.findElement(By.xpath("//*[contains(text(),'FINISH')]")).click();
+        Thread.sleep(2000);
+
+        //Assert.assertArrayEquals(driver.findElement(By.cssSelector("#checkout-complete_container"))).;
+        //Assert.assertTrue(driver.findElement(By.cssSelector("#checkout_complete_container")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#checkout_complete_container")).isDisplayed());
+        //Assert.assertArrayEquals(driver.findElement(By.cssSelector("#checkout-complete_container")));
     }
 
     @After
