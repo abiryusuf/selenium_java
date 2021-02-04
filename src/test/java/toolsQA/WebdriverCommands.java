8578cd3e9de5fd967b70208f304cfc8acb8d9844
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-03 20:46:45 -0500

    update

--- src/test/java/toolsQA/WebdriverCommands.java
+++ src/test/java/toolsQA/WebdriverCommands.java
@@ -39,6 +39,25 @@ public class WebdriverCommands {
         }
 
     }
+    @Test
+    public void checkBoxTest(){
+        driver.get("https://www.demoqa.com/automation-practice-form");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement ele = driver.findElement(By.cssSelector("label[for='hobbies-checkbox-1']"));
+        boolean isSelected = ele.isSelected();
+        if(isSelected == true){
+           ele.click();
+        }
+
+        WebElement ele1 = driver.findElement(By.xpath("//*[@for = 'hobbies-checkbox-2']"));
+        boolean isDisplay = ele1.isDisplayed();
+        if (isDisplay == true){
+            ele1.click();
+        }
+
+
+    }
 
     @After
     public void tearDown() throws Exception {
