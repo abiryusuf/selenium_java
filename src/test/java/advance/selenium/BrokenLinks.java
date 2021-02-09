d365046bbadf14f94d5f14bd74ebcde065e799d1
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-08 21:38:27 -0500

    broken links

--- src/test/java/advance/selenium/BrokenLinks.java
+++ src/test/java/advance/selenium/BrokenLinks.java
@@ -9,7 +9,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.Iterator;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class BrokenLinks {
@@ -31,14 +32,36 @@ public class BrokenLinks {
         List<WebElement> allURLS = driver.findElements(By.tagName("a"));
         System.out.println("Total link on the page " + allURLS.size());
 
-        Iterator<WebElement> iterator = allURLS.iterator();
-        while (iterator.hasNext()){
-            url = iterator.next().getText();
-            System.out.println(url);
+//        Iterator<WebElement> iterator = allURLS.iterator();
+//        while (iterator.hasNext()){
+//            url = iterator.next().getText();
+//            System.out.println(url);
+//        }
+        for (int i =0; i<allURLS.size(); i++){
+            WebElement ele = allURLS.get(i);
+            url = ele.getAttribute("href");
+            verifyLinks(url);
+
         }
 
 
 
+    }
+    public static void verifyLinks(String linkUrl){
+        try {
+            URL url = new URL(linkUrl);
+
+            HttpURLConnection http = (HttpURLConnection)url.openConnection();
+            http.setConnectTimeout(5000);
+            http.connect();
+            if (http.getResponseCode()>=400){
+                System.out.println(linkUrl + " " +http.getResponseMessage()+ "is a broken Links");
+            } else {
+                System.out.println(linkUrl + " " + http.getResponseMessage());
+            }
+        } catch (Exception e) {
+
+        }
     }
 
     @AfterClass
