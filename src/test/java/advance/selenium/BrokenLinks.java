04722c8e41052718e18d626f314bad6062f3a514
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-08 20:55:34 -0500

    broken links

---
+++ src/test/java/advance/selenium/BrokenLinks.java
@@ -0,0 +1,40 @@
+package advance.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class BrokenLinks {
+    WebDriver driver = null;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void brokenLinks(){
+        driver.get("https://demoqa.com/links");
+
+        String url = "";
+
+        List<WebElement> allURLS = driver.findElements(By.tagName("a"));
+        System.out.println("Total link on the page " + allURLS.get(1));
+
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
